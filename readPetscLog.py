e6ca66c47fc3dac99d63fe3aed04cff4c0bde611
Murat Keceli <keceli@gmail.com>
2014-02-20 12:52:09 -0600

    simple plot added

--- readPetscLog.py
+++ readPetscLog.py
@@ -32,5 +32,23 @@ def main():
         print matrixSize[i],nSlices[i], nProcessors[i], totalTime[i],filename[i]
         i=i+1
     print i, "files read"
+    series1x=[]
+    series1y=[]
+    series2x=[]
+    series2y=[]
+    series3x=[]
+    series3y=[]      
+    for i in len(nSlices):
+        if nProcessors[i]/nSlices[i]==4:
+            series1x.append(nProcessors[i])
+            series1y.append(totalTime[i])
+        if nProcessors[i]/nSlices[i]==8:
+            series2x.append(nProcessors[i])
+            series2y.append(totalTime[i])
+        if nProcessors[i]/nSlices[i]==16:
+            series3x.append(nProcessors[i])
+            series3y.append(totalTime[i])
+            
+    plt.plot(series1x,series1y)                   
 if __name__ == "__main__":
     main()
