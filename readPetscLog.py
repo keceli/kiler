4e9e2d5dbdaa140c8798c720f42a4545ad150079
Murat Keceli <keceli@gmail.com>
2014-02-20 18:18:33 -0600

    more plots

--- readPetscLog.py
+++ readPetscLog.py
@@ -1,4 +1,5 @@
-  
+def normalize(series):
+      return [series[0]/float(x) for x in series]
 def main():
     import glob
     import matplotlib.pyplot as plt
@@ -54,13 +55,32 @@ def main():
             series3x.append(nProcessors[i])
             series3y.append(totalTime[i])
  #           print "16 for slice",matrixSize[i],nSlices[i], nProcessors[i], totalTime[i],filename[i]
+ 
             
     plt.plot(series1x,series1y,linestyle='None', marker='o',color='k') 
     plt.plot(series2x,series2y,linestyle='None', marker='s',color='b') 
     plt.plot(series3x,series3y,linestyle='None', marker='+',color='g') 
     plt.xscale('log',basex=2)
     plt.yscale('log',basey=2)
+    plt.figure()
+    print series1x
+    print sorted(series1x)
+    print normalize(sorted(series1x))
+    print normalize(sorted(series1y,reverse=True))
+    efficiency1=[]
+    speedup1=[]
+    speedup1=normalize(sorted(series1y,reverse=True))
+    plt.plot(sorted(series1x),speedup1,linestyle='None',marker='o',color='k')
+    plt.xscale('log',basex=2)
+    plt.yscale('log',basey=2)
+    plt.figure()
+    efficiency1=[speedup1[i]*normalize(sorted(series1x))[i] for i in range(len(series1y))]
+    plt.plot(sorted(series1x),efficiency1,linestyle='None',marker='o',color='k')
+    plt.xscale('log',basex=2)
 
- #   plt.show()                  
+
+    
+    
+#    plt.show()                  
 if __name__ == "__main__":
     main()
